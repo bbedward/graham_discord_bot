efa806d80dae097078155671ea2ad7759639568e
Brandon Berhent <bbedward@gmail.com>
2019-11-19 16:29:24 -0500

    Add some encrypt/decrypt utils for seed storage

--- cli.py
+++ cli.py
@@ -2,10 +2,12 @@
 import argparse
 import sys
 import os
+import nanopy
 from tortoise import Tortoise, run_async
 from tortoise.utils import get_schema_sql
 from db.models.secret import Secret
-from util.random_util import RandomUtil
+from util.seed_util import SeedUtil
+from util.crypt_util import CryptUtil
 
 parser = argparse.ArgumentParser(description="Utilities for Graham TipBot")
 parser.add_argument('-i', '--init', action='store_true',  help='Initialize the bot')
@@ -29,6 +31,7 @@ async def init_db():
 async def init_seed_storage():
     await create_db() # connect
     has_seed = await Secret.all().count() > 0
+    seed = None
     if not has_seed:
         while True:
             ans = input("Do you want to import an existing seed? (y/n):")
@@ -37,11 +40,30 @@ async def init_seed_storage():
                 print("Please enter 'y' or 'n'")
                 continue
             elif ans_clean == 'n':
-                print("Generating seed...")
-                Secret()
+                seed = SeedUtil.generate_seed()
                 break
             elif ans_clean == 'y':
-                print("enter seed")
+                while True:
+                    seed_ans = input("Enter seed:")
+                    sanitized = seed_ans.strip().upper()
+                    if not SeedUtil.seed_is_valid(sanitized):
+                        print("Invalid seed, should be a 64 character hex-string")
+                        continue
+                    seed = sanitized
+                    break
+                break
+    # Get password to encrypt
+    password = None
+    while True:
+        password = input("Enter a password to encrypt this seed:")
+        if len(password.strip()) < 1:
+            print("bad password")
+            continue
+        break
+    crypt = CryptUtil(password)
+    print(crypt.encrypt(seed))
+
+
 
 if __name__ == '__main__':
     if options.init:
--- util/random_util.py
+++
@@ -1,8 +0,0 @@
-import string
-import secrets
-
-class RandomUtil():
-
-    @staticmethod
-    def generate_seed() -> str:
-        return ''.join(secrets.choice(string.hexdigits) for _ in range(64)).upper()
