d8217fb5468d05415efe84fd16747a39e5cae539
Brandon Berhent <bbedward@gmail.com>
2019-11-26 11:20:21 -0500

    Try a ReverseRelation

--- db/models/user.py
+++ db/models/user.py
@@ -16,6 +16,7 @@ class User(Model):
     name = fields.CharField(max_length=50)
     created_at = fields.DatetimeField(auto_now_add=True)
     modified_at = fields.DatetimeField(auto_now=True)
+    sent_transactions = fields.ReverseRelation('Transaction')
 
     class Meta:
         table = "users"
@@ -111,7 +112,7 @@ class User(Model):
     async def get_next_withdraw_s(self) -> int:
         """Get how long ago in seconds the user must wait until they can withdraw again"""
         # select * from transactions where user_id = ? and created_at > current_timestamp - WITHDRAW_COOLDOWN order by created_at desc limit 1
-        last_withdraw = await self.sent_transactions.filter(created_at__gt=datetime.datetime.utcnow() - datetime.timedelta(seconds=Constants.WITHDRAW_COOLDOWN)).order_by('-created_at').first()
+        last_withdraw = await self.sent_transactions.filter(created_at__gt=datetime.datetime.utcnow() - datetime.timedelta(seconds=Constants.WITHDRAW_COOLDOWN)).order_by('-sent_transactions__created_at').first()
         if last_withdraw is None:
             return -1
         # Get how many seconds until they can withdraw again
