7e8319afe656b10ceb81fb6675fdb95cd65a43a1
bbedward <bbedward@gmail.com>
2019-12-03 17:08:55 -0500

    This is quite a bit simpler

--- db/models/stats.py
+++ db/models/stats.py
@@ -1,4 +1,3 @@
-from aioredis_lock import RedisLock, LockTimeoutError
 from db.redis import RedisDB
 from tortoise.models import Model
 from tortoise.transactions import in_transaction
@@ -32,53 +31,18 @@ class Stats(Model):
     class Meta:
         unique_together = ('user', 'server_id')
 
-    async def purge_old_stats(self):
-        """Move total_tipped_amount to legacy_total_tipped_amount"""
-        try:
-            # Lock this so we don't do it twice
-            async with RedisLock(
-                await RedisDB.instance().get_redis(),
-                key=f"{Env.currency_symbol().lower()}purgestatslock",
-                timeout=300,
-                wait_timeout=300
-            ):
-                # TODO - this really sucks because tortoise doesn't do atomic updates...
-                # https://github.com/tortoise/tortoise-orm/issues/245
-                try:
-                    current_year = datetime.datetime.utcnow().year
-                    async with in_transaction() as conn:
-                        for s in await Stats.all():
-                            if s.stats_reset_at.year < current_year:
-                                s.legacy_total_tipped_amount += s.total_tipped_amount
-                                s.total_tipped_amount = 0
-                                await s.save(using_db=conn, update_fields=['legacy_total_tipped_amount', 'total_tipped_amount'])
-                except Exception:
-                    logging.getLogger().exception("exception purging old stats")
-                finally:
-                    await RedisDB.instance().delete(f"stats:reset:{self.stats_reset_at.year}")
-        except LockTimeoutError:
-            pass
-
-
     async def update_tip_stats(self, amount: float, giveaway: bool = False, rain: bool = False):
         # TODO - would be better to do these updates atomically
         # https://github.com/tortoise/tortoise-orm/issues/245
 
         # Reset ballers list if it's a new year
         if self.stats_reset_at.year < datetime.datetime.utcnow().year:
-            if not await RedisDB.instance().exists(f"stats:reset:{self.stats_reset_at.year}"):
-                await RedisDB.instance().set(f"stats:reset:{self.stats_reset_at.year}", expires=600)
-                await self.purge_old_stats()
-            else:
-                # Sleep for 60 seconds waiting for migration to complete
-                await asyncio.sleep(60)
-                if await RedisDB.instance().exists(f"stats:reset:{self.stats_reset_at.year}"):
-                    # Sleep for 60 more seconds
-                    await asyncio.sleep(60)
+            await Stats.all().update(total_tipped_amount=0, stats_reset_at=datetime.datetime.utcnow())
 
         # Update total tipped amount and count
         amount = NumberUtil.truncate_digits(amount, max_digits=Env.precision_digits())
         self.total_tipped_amount = NumberUtil.truncate_digits(float(self.total_tipped_amount) + amount, max_digits=Env.precision_digits())
+        self.legacy_total_tipped_amount = NumberUtil.truncate_digits(float(self.legacy_total_tipped_amount) + amount, max_digits=Env.precision_digits())
         self.total_tips += 1
         # Update all time tip if necessary
         top_tip_updated = False
@@ -110,7 +74,7 @@ class Stats(Model):
             giveaway_updated = True
 
         async with in_transaction() as conn:
-            update_fields = ['total_tipped_amount', 'total_tips']
+            update_fields = ['total_tipped_amount', 'legacy_total_tipped_amount', 'total_tips']
             if top_tip_updated:
                 update_fields.append('top_tip')
                 update_fields.append('top_tip_at')
