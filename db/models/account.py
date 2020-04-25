5478a9b0564a4a4cf2fbab08c4e40e73d2a15c35
Brandon Berhent <bbedward@gmail.com>
2020-04-25 14:21:44 -0400

    Don't need to specify unique kwarg on OneToOneField

--- db/models/account.py
+++ db/models/account.py
@@ -2,7 +2,7 @@ from tortoise.models import Model
 from tortoise import fields
 
 class Account(Model):
-    user = fields.OneToOneField('db.User', related_name='account', unique=True, index=True)
+    user = fields.OneToOneField('db.User', related_name='account', index=True)
     address = fields.CharField(max_length=65, unique=True, index=True)
 
     class Meta:
